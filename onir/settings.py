da6232830b93763c7df421077965491c7a98af79
Jakub Karolak <jakub.karolak90@gmail.com>
2022-08-01 23:47:42 +0200

    second commit

--- onir/settings.py
+++ onir/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v+-ydv%$z8$^8&d9w)&)%2cgd0bvv0($#^a_a=+ugd_v!$7sa7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
