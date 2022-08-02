aa67514b2b02fb59a18d7c4e0c0ddce8f5bd4602
Jakub Karolak <jakub.karolak90@gmail.com>
2022-08-02 22:26:08 +0200

    Added null parameters for Match objects

--- team/views.py
+++ team/views.py
@@ -4,4 +4,10 @@ from .models import MatchPlayer
 
 # Create your views here.
 def my_matches(request):
+
+    information_list = (
+
+    )
+    for match_pl
+
     return render(request, 'my_matches.html', {'MatchPlayer':MatchPlayer.objects.all()})
