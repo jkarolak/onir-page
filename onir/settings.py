35d6306307e8d4dc6f8037990299f78a54661277
Jakub Karolak <jakub.karolak90@gmail.com>
2022-08-02 22:07:36 +0200

    small changes vol. 2

--- onir/settings.py
+++ onir/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'onir.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templatesdsdsd"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
