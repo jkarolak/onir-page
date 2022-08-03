f3903d773db433a30f9e2627089e6ffc6681eaeb
Jakub Karolak <jakub.karolak90@gmail.com>
2022-08-03 02:35:02 +0200

    GoodChanges

--- team/urls.py
+++ team/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from team.views import my_matches
+from . import views
+
 
 urlpatterns = [
-    path('my_matches/', my_matches)
+    path('my_matches/', my_matches),
+    path('present_button/<int:pk>/', views.present_button, name="present_button"),
+    path('absent_button/<int:pk>/', views.absent_button, name="absent_button"),
+    path('refresh_match_player_button/', views.refreshMatchPlayer, name="refresh_match_player_button")
+
 ]
 
