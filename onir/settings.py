685a36552ba1645d1ac337000b6fe3d51b172404
Jakub Karolak <jakub.karolak90@gmail.com>
2022-08-14 15:26:38 +0200

    debug na false

--- onir/settings.py
+++ onir/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v+-ydv%$z8$^8&d9w)&)%2cgd0bvv0($#^a_a=+ugd_v!$7sa7
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGIN_REDIRECT_URL = '/team/my_matches/'
 LOGIN_URL='login'
