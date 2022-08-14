de49a426a6eede7c3104a3d0d5b288b5f223e530
Jakub Karolak <jakub.karolak90@gmail.com>
2022-08-14 13:46:06 +0200

    automatyzacja generowania meczyków

--- team/models.py
+++ team/models.py
@@ -1,6 +1,8 @@
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import (post_save)
+from django.dispatch import receiver
 
 
 AVAILABILITIES_STATUS = (
@@ -46,3 +48,11 @@ class MatchPlayer(models.Model):
                 if MatchPlayer.objects.filter(player=player, match=match).count()==0:
                     MatchPlayer.objects.create(player=player, match=match)
 
+
+@receiver(post_save,sender=Match)
+def refresh_match_player_list(*args,**kwargs):
+    MatchPlayer.refreshMatchPlayer()
+
+@receiver(post_save,sender=Player)
+def refresh_match_player_list(*args,**kwargs):
+    MatchPlayer.refreshMatchPlayer()
